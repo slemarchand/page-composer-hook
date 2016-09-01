3de2fd91428bdcea83395dc4057ba098e10959db
Sebastien Le Marchand <sebastien.lemarchand@gmail.com>
2016-09-02 01:59:32 +0200

    More robust javascript

--- docroot/custom_jsps/html/js/page_composer/page_body_bottom.js
+++ docroot/custom_jsps/html/js/page_composer/page_body_bottom.js
@@ -4,20 +4,24 @@
 		
 		var clearThemeSwitchLink = document.querySelector('#clearThemeSwitchLink');
 		
-		clearThemeSwitchLink.addEventListener("click", function(event) {
-			
-			event.preventDefault();
-			
-			var httpRequest = new XMLHttpRequest()
-			httpRequest.onreadystatechange = function () {
-				console.log('reload');
-				location.href = location.href
-								.replace(/_pagecomposer_[^&]*&?/g, '')
-								.replace(/\\?$/g, '');
-			}
-			httpRequest.open('POST', '/c/page_composer/clear_theme_switch', true);
-			httpRequest.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-			httpRequest.send('');
-		});
+		if(clearThemeSwitchLink && clearThemeSwitchLink != null) {
+		
+			clearThemeSwitchLink.addEventListener("click", function(event) {
+				
+				event.preventDefault();
+				
+				var httpRequest = new XMLHttpRequest()
+				httpRequest.onreadystatechange = function () {
+					console.log('reload');
+					location.href = location.href
+									.replace(/_pagecomposer_[^&]*&?/g, '')
+									.replace(/\\?$/g, '');
+				}
+				httpRequest.open('POST', '/c/page_composer/clear_theme_switch', true);
+				httpRequest.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+				httpRequest.send('');
+			});
+		
+		}
 	});
 })();
